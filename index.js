72925792f9dbc1c6a25b1785a913ccfef8086248
Andrew Shaffer <andrew@weo.io>
2015-07-09 15:02:56 -0700

    fix canvas to use toDataURL

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ var imgToCanvas = require('img-to-canvas')
 var toBlob = require('canvas-to-blob')
 
 module.exports = function(img, x, y, width, height) {
-  return toBlob(imgToCanvas(img, x, y, width, height))
+  return toBlob(imgToCanvas(img, x, y, width, height).toDataURL('image/png'))
 }
